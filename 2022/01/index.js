ceca979895c543a307b7d7d8ec36f06d95ac0576
Jack Farrell <jackfarrell3030@gmail.com>
2023-09-10 19:38:06 -0500

    update comments

--- 2022/01/index.js
+++ 2022/01/index.js
@@ -1,7 +1,39 @@
 import { getInput } from "../../utils/index.js";
-const os = require('node:os');
 
-//~\Development\advent-of-code-js> npm config set script-shell `C:\Program Files\nu\bin\nu.exe`
+// credit for the answers below go to zsoltime's repo https://github.com/zsoltime/advent-of-code
+const initialValue = 0;
+const sum = (nums) => nums.reduce((a, b) => a + b, initialValue);
+
+function getCaloriesByElves(input) {
+	return input
+		.split('\r\n\r\n')
+		.map( (line) => line.split('\n').map(Number))
+		.map(sum);
+}
+
+function getMaxCalories(input) {
+	return Math.max(...getCaloriesByElves(input));
+}
+
+function getTopThreeCalories(input) {
+	const top3 = getCaloriesByElves(input)
+		.sort((a,b) => b - a)
+		.slice(0,3);
+	return sum(top3);
+}
+	
+
+const input = getInput(import.meta.url);
+
+const ans1 = getMaxCalories(input);
+
+const ans2 = getTopThreeCalories(input);
+
+console.log(`
+	Answer One: ${ans1}
+	Answer Two: ${ans2}
+`);
+
 //async function getMaxCals(fileName) {
 //	let sumValues = [];
 //	let arr = [];
@@ -62,48 +94,4 @@ const os = require('node:os');
 //		
 //	} catch (err) {
 //		console.error(err);
-//	}
 //}
-
-// credit for the answers below go to zsoltime's repo https://github.com/zsoltime/advent-of-code
-const initialValue = 0;
-const sum = (nums) => nums.reduce((a, b) => a + b, initialValue);
-
-function getCaloriesByElves(input) {
-	const arr = input.split(/\r?\n/);
-	console.log(arr.length);
-	return input
-		.split(os.EOL)
-		.map( (line) => line.split('\n').map(Number))
-		.map(sum);
-}
-
-function getMaxCalories(input) {
-	return Math.max(...getCaloriesByElves(input));
-}
-
-function getTopThreeCalories(input) {
-	const top3 = getCaloriesByElves(input)
-		.sort((a,b) => b - a)
-		.slice(0,3);
-	return sum(top3);
-}
-	
-
-const input = getInput(import.meta.url);
-
-const ans1 = getMaxCalories(input);
-
-const ans2 = getTopThreeCalories(input);
-
-console.log(`
-	Answer One: ${ans1}
-	Answer Two: ${ans2}
-`);
-
-
-
-//getMaxCals(input);
-
-//getTop3MaxCals(fileName);
-
