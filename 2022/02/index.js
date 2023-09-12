18489817bac50ea32ecfa305ad89b323251f2c60
Jack Farrell <jackfarrell3030@gmail.com>
2023-09-12 18:44:00 -0500

    complete day2p2

--- 2022/02/index.js
+++ 2022/02/index.js
@@ -20,6 +20,12 @@ const playerChoice = {
     'Z': 3
 }
 
+const playerChoiceP2 = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6
+}
+
 function determineWinner(opponent, player) {
     switch(opponent){
         case 'A':
@@ -55,35 +61,88 @@ function determineWinner(opponent, player) {
     }
 }
 
-function calculateRound(roundString) {
+function calculateRoundAns1(roundString) {
     let roundArr = roundString.split(' ');
-    let result = determineWinner(String(roundArr[0]), String(roundArr[1]));
+    let result = determineWinner(roundArr[0], roundArr[1]);
     if (result > 0) {
-        return Number(playerChoice[roundArr[1]]) + 6;
+        return playerChoice[roundArr[1]] + 6;
     }
     else if (result == 0) {
-        return Number(playerChoice[roundArr[1]]) + 3;
+        return playerChoice[roundArr[1]] + 3;
     }
     else {
-        return Number(playerChoice[roundArr[1]]);
+        return playerChoice[roundArr[1]];
     }
 }
 
-function processRounds(inputArr) {
+function determineChoice(opponent, player) {
+    switch(player) {
+        //lose
+        case 'X':
+            if(opponent == 'A'){
+                return 3;
+            }
+            else if (opponent == 'B'){
+                return 1;
+            }
+            else if (opponent == 'C'){
+                return 2;
+            }
+        //draw 
+        case 'Y':
+            if(opponent == 'A'){
+                return 1;
+            }
+            else if (opponent == 'B'){
+                return 2;
+            }
+            else if (opponent == 'C'){
+                return 3;
+            }
+        //win
+        case 'Z':
+            if(opponent == 'A'){
+                return 2;
+            }
+            else if (opponent == 'B'){
+                return 3;
+            }
+            else if (opponent == 'C'){
+                return 1;
+            }
+    }
+
+}
+
+function calculateRoundAns2(roundString) {
+    let roundArr = roundString.split(' ');
+    return determineChoice(roundArr[0], roundArr[1]) + playerChoiceP2[roundArr[1]];
+}
+
+function processRounds(inputArr, callback) {
     let sum = 0;
-    inputArr.forEach(element => {
-        if (element.trim().length !== 0){
-            sum += calculateRound(element);
+    inputArr.forEach(ele => {
+        if (ele.trim().length !== 0){
+            sum += callback(ele);
         }
     });
     return sum;
 }
 
 const input = getInput(import.meta.url);
-let inputArr = input.split(os.EOL);
+const inputArr = input.split(os.EOL);
 
-let answer = processRounds(inputArr);
+const answer1 = processRounds(inputArr, calculateRoundAns1);
+
+/*Part 2
+ * X: need to lose
+ * Y: need to draw
+ * Z: need to win
+*/
+
+const answer2 = processRounds(inputArr, calculateRoundAns2);
 
 console.log(`
-    Answer: ${answer}
+    Answer 1: ${answer1}
+    Answer 2: ${answer2}
 `);
