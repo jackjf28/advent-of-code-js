2d6925a6d82ffd0fb80e4e86a67c4129deb0382c
Jack Farrell <jackfarrell3030@gmail.com>
2023-09-13 20:58:28 -0500

    finish day 3 part 1

--- 2022/03/index.js
+++ 2022/03/index.js
@@ -1,9 +1,44 @@
-import { getInput } from "../../utils/index.js";
+import { getInput, splitLines } from "../../utils/index.js";
 
+function getPriority(char) {
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return letters.indexOf(char) + 1;
+    
+}
 
+function processRucksack(rucksack) {
+    const half = Math.ceil(rucksack.length/2);
+    const con1 = rucksack.slice(0,half).trim().split('');
+    const con2 = rucksack.slice(half).trim().split('');
+    let commonChar;
+    let isFound = false;
+    for (let i = 0; i < con1.length; i++)
+    {
+        for (let j = 0; j < con2.length; j++)
+        {
+            if (con1[i] === con2[j])
+            {
+                isFound = true;
+                commonChar = con1[i];
+                break;
+            }
+        }
+        if (isFound) {break;}
+    }
+    return getPriority(commonChar);
+}
 
-
-
+function getSumOfCommonItems(input) {
+    let arr = splitLines(input);
+    let sum = arr.reduce((acc,element) => acc + processRucksack(element),0);
+    return sum;
+}
 
 const input = getInput(import.meta.url);
+const ans1 = getSumOfCommonItems(input);
+const ans2 = null;
 
+console.log(`
+Answer 1: ${ans1}
+Answer 2: ${ans2}
+`);
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import os from "os";
 
 export function getInput(moduleUrl) {
 	const __filename = fileURLToPath(moduleUrl);
@@ -8,4 +9,9 @@ export function getInput(moduleUrl) {
 	return readFileSync(resolve(__dirname, './input.txt'), {
 		encoding: 'utf-8',
 	});
+}
+
+
+export function splitLines(input){
+	return input.trim().split(os.EOL);
 }
