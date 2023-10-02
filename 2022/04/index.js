a85566fb42c1af04b1b78ae03fb52ddf3f5f4146
Jack Farrell <jackfarrell3030@gmail.com>
2023-10-01 20:54:41 -0500

    add day 4 starter

---
+++ 2022/04/index.js
@@ -0,0 +1,10 @@
+import { getInput, splitLines} from "../../utils/index.js"
+
+const input = getInput(import.meta.url);
+const ans1 = null;
+const ans2 = null;
+
+console.log(`
+	Answer 1: ${ans1}
+	Answer 2: ${ans2}
+`);
